90db6286cdfb493de5dc944783aa65b4fbce38b8
Ask Solem <askh@opera.com>
2010-02-21 14:58:54 +0100

    Use rpop/rpush instead of deprecated pop/push (conforming to redis 1.x)

--- ghettoq/backends/pyredis.py
+++ ghettoq/backends/pyredis.py
@@ -1,18 +1,21 @@
 from redis import Redis as Redis
 from ghettoq.backends.base import BaseBackend
 
+DEFAULT_PORT = 6379
+
 
 class RedisBackend(BaseBackend):
 
     def establish_connection(self):
+        self.port = self.port or DEFAULT_PORT
         return Redis(host=self.host, port=self.port, db=self.database,
                      password=self.password)
 
     def put(self, queue, message):
-        self.client.push(queue, message, head=False)
+        self.client.lpush(queue, message)
 
     def get(self, queue):
-        return self.client.pop(queue)
+        return self.client.rpop(queue)
 
     def purge(self, queue):
         return self.client.delete(queue)
