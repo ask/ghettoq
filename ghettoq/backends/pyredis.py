1eec5de4ccff5e4d87fb2ea00d9ff1cd0089ec7d
Ask Solem <askh@opera.com>
2010-06-02 18:11:41 +0200

    PEP8 + cosmetic changes.

--- ghettoq/backends/pyredis.py
+++ ghettoq/backends/pyredis.py
@@ -2,29 +2,29 @@ from redis import Redis as Redis
 from ghettoq.backends.base import BaseBackend
 
 DEFAULT_PORT = 6379
-DEFAULT_DB   = 0
+DEFAULT_DB = 0
+
 
 class RedisBackend(BaseBackend):
 
     def __init__(self, host=None, port=None, user=None, password=None,
             database=None, timeout=None):
-        
-        if type(database) != int:
+
+        if not isinstance(database, int):
             if not database or database == "/":
                 database = DEFAULT_DB
             elif database.startswith('/'):
-                try:
-                     database = int(database[1:])
-                except ValueError:
-                     raise AttributeError('Database name must be integer between 0 and database_count - 1')
-            else:
-                try:
-                     database = int(database)
-                except ValueError:
-                     raise AttributeError('Database name must be integer between 0 and database_count - 1')
-        
-        super(RedisBackend, self).__init__(host, port, user, password, database, timeout)
-        
+                database = database[1:]
+            try:
+                database = int(database)
+            except ValueError:
+                raise AttributeError(
+                    "Database name must be integer between 0 "
+                    "and database_count - 1")
+
+        super(RedisBackend, self).__init__(host, port, user, password,
+                                           database, timeout)
+
     def establish_connection(self):
         self.port = self.port or DEFAULT_PORT
         return Redis(host=self.host, port=self.port, db=self.database,
